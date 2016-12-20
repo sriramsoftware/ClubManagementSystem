2d36568fc5d369b7c94136fe78e7bc2bcdd8d187
Khaled Alam <khaledalam.net@gmail.com>
2016-12-20 04:54:20 +0200

    init commit ever

--- GYM Management MetroUI/UI/ManagementForms/MainForm.cs
+++ GYM Management MetroUI/UI/ManagementForms/MainForm.cs
@@ -34,7 +34,7 @@ namespace GYM_Management_MetroUI
         private void MainForm_Load(object sender, EventArgs e)
         {
             // set auto resfresh for attendance
-            // backcoloring cells depending on text inside dynamically ex: {
+            // backcoloring cells depending on text inside dynamically ex:  {
             //      "still here" => (light green for current cell)  
             //      "leave" => (light red for whole row)
             // } 
